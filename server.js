410f5f554c0b0e47256d4ace8f08941fb4e8173c
Traceyafk <morrit29@gmatc.matc.edu>
2021-12-01 17:42:08 -0600

    Completed Activity 9

--- server.js
+++ server.js
@@ -7,149 +7,189 @@ import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from '.models/User';
 import Post from '.models/Post';
-import auth from './middleware/auth';
+import auth from './middleware/auth'
+import path from 'path';
+
+app.post('/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password',
+            'Please enter a password with 6 or more characters')
+            .isLength({ min: 6 }),
 
 
-// Initialize express application
-const app = express();
+    ],
+    // Callback method
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { name, email, password } = req.body;
+            try {
+                // Check if user exists
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'User already exists' }] });
 
-// Connect database
-connectDatabase();
+                }
 
-// Configure Middleware
-app.use(express.json({ extended: false }));
-app.use(
-  cors({
-    origin: 'http://localhost:3000'
-  })
-);
+                // Create a new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
 
-// API endpoints
-/**
- * @route POST api/users
- * @desc Register user
- */
-app.post(
-  '/api/users',
-  [
-    check('name', 'Please enter your name')
-      .not()
-      .isEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 })
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    } else {
-      const { name, email, password } = req.body;
-      try {
-        // Check if user exists
-        let user = await User.findOne({ email: email });
-        if (user) {
-          return res
-            .status(400)
-            .json({ errors: [{ msg: 'User already exists' }] });
+                });
+
+                // Encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                // Save to the db and return: mongoose function
+                await user.save();
+
+                // Generate and return a JWT token
+                returnToken(user, res);
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
         }
-
-        // Create a new user
-        user = new User({
-          name: name,
-          email: email,
-          password: password
-        });
-
-        // Encrypt the password
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-
-        // Save to the db and return
-        await user.save();
-
-        // Generate and return a JWT token
-        returnToken(user, res);
-      } catch (error) {
-        res.status(500).send('Server error');
-      }
     }
-  }
 );
-
 /**
- * @route GET api/auth
- * @desc Authorize user
- */
-app.get('/api/auth', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).send('Unknown server error');
-  }
+ * @route Get api/auth
+ * @desc Authenticate user
+  */
+ app.get('/api/auth', auth, async (req, res) => {
+
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send('Unknown server error');
+    }
 });
+/**
+ * @route Post api/login
+ * @desc Login user
+  */
+app.post(
+    '/api/login',
+    [
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'A password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { email, password } = req.body;
+            try {
+                // Check if user exists
+                let user = await User.findOne({ email: email });
+                if (!user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+
+                }
+
+                // Check password
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
+
+
+                // Generate and return a JWT token
+                returnToken(user, res);
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
 
 /**
- * @route POST api/login
- * @desc Login user
- */
-app.post(
-  '/api/login',
-  [
-    check('email', 'Please enter a valid email').isEmail(),
-    check('password', 'A password is required').exists()
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    } else {
-      const { email, password } = req.body;
-      try {
-        // Check if user exists
-        let user = await User.findOne({ email: email });
-        if (!user) {
-          return res
-            .status(400)
-            .json({ errors: [{ msg: 'Invalid email or password' }] });
-        }
+ * @route Get api/auth
+ * @desc Authenticate user
+  */
+ app.get('/api/auth', auth, async (req, res) => {
 
-        // Check password
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return res
-            .status(400)
-            .json({ errors: [{ msg: 'Invalid email or password' }] });
-        }
-
-        // Generate and return a JWT token
-        returnToken(user, res);
-      } catch (error) {
-        res.status(500).send('Server error');
-      }
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send('Unknown server error');
     }
-  }
+});
+/**
+ * @route Post api/login
+ * @desc Login user
+  */
+ app.post(
+    '/api/login',
+    [
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'A password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { email, password } = req.body;
+            try {
+                // Check if user exists
+                let user = await User.findOne({ email: email });
+                if (!user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+
+                }
+
+                // Check password
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
+
+
+                // Generate and return a JWT token
+                returnToken(user, res);
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
+    }
 );
 
 const returnToken = (user, res) => {
-  const payload = {
-    user: {
-      id: user.id
-    }
-  };
+    const payload = {
+        user: {
+            id: user.id
+        }
+    };
 
-  jwt.sign(
-    payload,
-    config.get('jwtSecret'),
-    { expiresIn: '10hr' },
-    (err, token) => {
-      if (err) throw err;
-      res.json({ token: token });
-    }
-  );
+    jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '10hr' },
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+        }
+    );
 };
 
 // Post endpoints
@@ -157,154 +197,159 @@ const returnToken = (user, res) => {
  * @route POST api/posts
  * @desc Create post
  */
-app.post(
-  '/api/posts',
-  [
+app.post
+('/api/posts',
+[
     auth,
     [
-      check('title', 'Title text is required')
+        check('title', 'Title text is required')
         .not()
         .isEmpty(),
-      check('body', 'Body text is required')
+        check('body','Body text is required')
         .not()
         .isEmpty()
+
     ]
-  ],
-  async (req, res) => {
+],
+async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty()){
+        res.status(422).json({ errors: errors.array() });
     } else {
-      const { title, body } = req.body;
-      try {
-        // Get the user who created the post
-        const user = await User.findById(req.user.id);
+        const { title, body } = req.body;
+        try {
+            // Get the user who created the post
+            const user = await User.findById(req.user.id);
 
-        // Create a new post
-        const post = new Post({
-          user: user.id,
-          title: title,
-          body: body
-        });
+            // Create a new post
+            const post = new Post({
+                user: user.id,
+                title: title,
+                body: body
+            });
 
-        // Save to the db and return
-        await post.save();
+            // Save to the db and return 
+            await post.save();
 
-        res.json(post);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Server error');
-      }
+            res.json(post);   
+          } catch (error) {
+            console.error(error); 
+            res.status(500).send('Server error'); 
+
+        }
     }
-  }
-);
-
+});
 /**
+ * 
  * @route GET api/posts
  * @desc Get posts
- */
+ **/
+
 app.get('/api/posts', auth, async (req, res) => {
-  try {
-    const posts = await Post.find().sort({ date: -1 });
+    try {
+        const posts = await Post.find().sort({ date: -1 });
 
-    res.json(posts);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
+        res.json(posts);
+
+    }catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+
+    }
 });
-
 /**
- * @route GET api/posts/:id
+ * 
+ *@route Get api/posts/:id
  * @desc Get post
  */
-app.get('/api/posts', auth, async (req, res) => {
-  try {
-    const post = await Post.find(req.params.id);
 
-    // Make sure the post was found
-    if (!post) {
-      return res.status(404).json({ msg: 'Post not found' });
-    }
+ app.get('/api/posts/:id', auth, async (req, res) => {
+     try {
+         const post = await Post.findById(req.params.id);
 
-    res.json(post);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
-});
+         // Make sure the post was found
+         if (!post) {
+             return res.status(404).json({ msg: 'Post not found' });
 
-/**
- * @route DELETE api/posts/:id
- * @desc Delete a post
- */
-app.delete('/api/posts/:id', auth, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
+         }
+         res.json(post);
+     } catch (error) {
+         console.error(error);
+         res.status(500).send('Server error');
 
-    // Make sure the post was found
-    if (!post) {
-      return res.status(404).json({ msg: 'Post not found' });
-    }
+     }
+ });
+ /**
+  * 
+  * @route DELETE api/posts/:id 
+  * @Delete a post 
+  */
 
-    // Make sure the request user created the post
-    if (post.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' });
-    }
+ app.delete('/api/posts/:id', auth, async (req, res) => {
+     try {
+         const post = await Post.findById(req.params.id);
 
-    await post.remove();
+         // Make sure the post was found
+         if (!post) {
+             return res.status(404).json({ msg: 'Post not found' });
+         }
+         // Make sure the request user created the post
+         if (post.user.toString() !== req.user.id) {
+             return res.status(401).json({ msg: 'User not authorized' });
+         }
+         await post.remove();
 
-    res.json({ msg: 'Post removed' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
-});
+         res.json({ msg: 'Post deleted successfully' });
+     }   catch (error) {
+         console.error(error);
+         res.status(500).send('Server error');
+     }
+ });
+ /**
+  * 
+  * @route PUT api/posts/:id 
+  * @desc Update a post
+  */
+ app.put ('/api/posts/:id', auth, async (req, res) => {
+     try {
+         const { title, body } = req.body;
+         const post = await Post.findById(req.params.id);
 
-/**
- * @route PUT api/posts/:id
- * @desc Update a post
- */
-app.put('/api/posts/:id', auth, async (req, res) => {
-  try {
-    const { title, body } = req.body;
-    const post = await Post.findById(req.params.id);
+         // Make sure the post was found
+         if (!post) {
+             return res.status(404).json({ msg: 'Post not found' });
+         }
+         // Make sure the request user created the post
+         if (post.user.toString() !== req.user.id) {
+             return res.status(401).json({ msg: 'User not authorized' });
+         }
 
-    // Make sure the post was found
-    if (!post) {
-      return res.status(404).json({ msg: 'Post not found' });
-    }
+         // Update the post and return
+         post.title = title || post.title;
+         post.body = body || post.body;
 
-    // Make sure the request user created the post
-    if (post.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' });
-    }
+         await post.save();
 
-    // Update the post and return
-    post.title = title || post.title;
-    post.body = body || post.body;
+         res.json(post);
+     }   catch (error) {
+         console.error(error);
+         res.status(500).send('Server error');
 
-    await post.save();
-
-    res.json(post);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
-});
+     }
+ });
 
 // Serve build files in production
 if (process.env.NODE_ENV === 'production') {
-  // Set the build folder
-  app.use(express.static('client/build'));
+    // Set the build folder
+    app.use(express.static('client/build'));
 
-  // Route all requests to serve up the built index file
-  // (i.e., [current working directory]/client/build/index.html)
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    // Route all requests to serve up the built index file
+    // (i.e. [current working directory]/client/build/index.html)
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
-// Connection listener
+//Connection listener
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
